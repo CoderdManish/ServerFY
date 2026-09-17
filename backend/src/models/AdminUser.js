02db3b31ba824fe4a1037518b2e340ac6882ecc8
CoderdManish <94036559+CoderdManish@users.noreply.github.com>
2026-09-17 06:10:40 +0000

    Changes
    Co-authored-by: CoderdManish <94036559+CoderdManish@users.noreply.github.com>

--- backend/src/models/AdminUser.js
+++ backend/src/models/AdminUser.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export const PERMISSIONS = ["analytics", "leads", "requests", "admins"];
+export const PERMISSIONS = ["analytics", "leads", "requests", "admins", "blog"];
 
 const adminUserSchema = new mongoose.Schema(
   {
